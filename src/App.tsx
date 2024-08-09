840e0fe3e837be8ef5249510b3981a3e3d2e3841
marcello-f <fredericks.marcello@gmail.com>
2024-08-09 12:42:14 +0100

    add appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import ButtonAppBar from "./components/Appbar";
 
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar></ButtonAppBar>
       <header className="App-header">
         <h1>Hello World!</h1>
       </header>
